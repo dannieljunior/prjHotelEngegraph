00bda65fa0bbf1cfaa25dbc18777c8195048b40f
dannieljunior <danniel.prg@gmail.com>
2022-08-13 12:00:19 -0300

    CRUD de exemplo ADO.Net

--- slnPrjHotelEngegraph/Engegraph.ADO/Program.cs
+++ slnPrjHotelEngegraph/Engegraph.ADO/Program.cs
@@ -12,51 +12,72 @@ namespace Engegraph.ADO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ACESSANDO DADOS COM ADO.NET");
-
             try
             {
-                var ID = "75F7C398-752C-4D33-9439-8BA7D72174E8";
-
-                var sql = $"SELECT * FROM TIPOUH WHERE ID = @ID";
-
-                using (var conexao = new SqlConnection())
+                using (var conexao = new SqlConnection(@"Server=2K21-DELL\SQLEXPRESS;Database=HotelEngegraph;User=sa;Password=123456"))
                 {
-                    conexao.ConnectionString = @"Server=2K21-DELL\SQLEXPRESS;Database=HotelEngegraph;User=sa;Password=123456";
-                    var comando = new SqlCommand(sql, conexao);
-                    comando.Parameters.AddWithValue("ID", ID);
-                    conexao.Open();
-                    Console.WriteLine("conectado ao banco de dados");
+                    
+                    Console.WriteLine("Meu primeiro CRUD completo");
 
-                    List<TipoUh> tiposUh = new List<TipoUh>();
+                    var crud = new TipoUhCrud(conexao);
 
-                    using (var reader = comando.ExecuteReader())
+                    var tipoUh = new TipoUh();
+
+                    tipoUh.Id = new Guid("9bf65609-1c0f-4bfb-be31-4c633847a5ac");
+                    tipoUh.Descricao = "Super Lux Suite";
+                    tipoUh.QtdeAdt = 2;
+                    tipoUh.QtdeChd = 2;
+                    tipoUh.ValorDiaria = 499.00;
+                    tipoUh.ValorAdicional = 699.00;
+
+                    crud.Create(tipoUh);
+
+                    Console.WriteLine("Dados inseridos com sucesso");
+
+                    Console.WriteLine("Lendo dados do banco ...");
+
+                    var dados = crud.Read(new FiltroTipoUH() { Id = "75F7C398-752C-4D33-9439-8BA7D72174E8", Descricao = "S" });
+
+                    foreach (var item in dados)
                     {
-                        while (reader.Read())
-                        {
-                            tiposUh.Add(new TipoUh()
-                            {
-                                Id = (Guid)reader["Id"],
-                                Descricao = reader["Descricao"].ToString(),
-                                ValorDiaria = Convert.ToDouble(reader["ValorDiaria"])
-                            });
-                        }
+                        Console.WriteLine($"Id: {item.Id}");
+                        Console.WriteLine($"Descricao: {item.Descricao}");
+                        Console.WriteLine($"QtdeAdt: {item.QtdeAdt}");
+                        Console.WriteLine($"QtdeChd: {item.QtdeChd}");
+                        Console.WriteLine($"ValorDiaria: {item.ValorDiaria}");
+                        Console.WriteLine($"ValorAdicional: {item.ValorAdicional}");
+                        Console.WriteLine($"DataCriacao: {item.DataCriacao}");
+                        Console.WriteLine($"DataModificacao: {item.DataModificacao}");
+                        Console.WriteLine("---------------------------------------------------------------------------------------");
                     }
 
-                    foreach (var item in tiposUh)
-                    {
-                        Console.WriteLine($"Descrição: {item.Descricao}; Valor Diária: R$ {item.ValorDiaria.ToString("0.00")}");
-                    }
+
+                    tipoUh.Id = new Guid("9bf65609-1c0f-4bfb-be31-4c633847a5ac");
+                    tipoUh.Descricao = "SUPER LUX SUITE";
+                    tipoUh.QtdeAdt = 2;
+                    tipoUh.QtdeChd = 2;
+                    tipoUh.ValorDiaria = 699.00;
+                    tipoUh.ValorAdicional = 899.00;
+
+                    crud.Update(tipoUh);
+
+                    var id = new Guid("9bf65609-1c0f-4bfb-be31-4c633847a5ac");
+
+                    crud.Delete(id);
+
+                    Console.WriteLine("Registro excluído com sucesso!"); 
                 }
+
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine(ex.Message);
             }
-
-            Console.WriteLine("Leitura concluída com sucesso");
-            Console.ReadKey();
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
---
+++ slnPrjHotelEngegraph/Engegraph.ADO/TipoUhCrud.cs
@@ -0,0 +1,186 @@
+﻿using Hotel.Comum.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engegraph.ADO
+{
+    enum EnOperacao
+    {
+        Insert = 0,
+        Update = 1,
+        Delete = 2
+    }
+
+    class FiltroTipoUH
+    {
+        public string Id { get; set; }
+        public string Descricao { get; set; } 
+    }
+
+    //CRUD
+
+    class TipoUhCrud
+    {
+        private readonly SqlConnection _conexao;
+
+        public TipoUhCrud(SqlConnection pConexao)
+        {
+            _conexao = pConexao;
+            _conexao.Open();
+        }
+
+        private void ExecutarComando(string sql, TipoUh pTipoUh, EnOperacao pOperacao = EnOperacao.Insert)
+        {
+            //instancia um novo SqlCommand
+            var comando = CriarComando(sql);
+
+            //Adiciona parametros com o seus respectivos valores
+            comando.Parameters.AddWithValue("Id", pTipoUh.Id);
+
+            if(pOperacao != EnOperacao.Delete)
+            {
+                comando.Parameters.AddWithValue("Descricao", pTipoUh.Descricao);
+                comando.Parameters.AddWithValue("QtdeAdt", pTipoUh.QtdeAdt);
+                comando.Parameters.AddWithValue("QtdeChd", pTipoUh.QtdeChd);
+                comando.Parameters.AddWithValue("ValorDiaria", pTipoUh.ValorDiaria);
+                comando.Parameters.AddWithValue("ValorAdicional", pTipoUh.ValorAdicional);
+
+                var agora = DateTime.Now;
+
+                if (pOperacao == EnOperacao.Insert)
+                    comando.Parameters.AddWithValue("DataCriacao", agora);
+                else
+                    comando.Parameters.AddWithValue("DataModificacao", agora);
+            }
+
+            //Executa um comando de NÃO consulta
+            comando.ExecuteNonQuery();
+        }
+
+        private SqlCommand CriarComando(string pSql)
+        {
+            var comando = _conexao.CreateCommand();
+            comando.CommandText = pSql;
+            return comando;
+        }
+
+
+        public void Create(TipoUh pTipoUh)
+        {
+            //comando sql de insert
+            var sql = @"INSERT INTO TipoUh
+                               (Id
+                               ,Descricao
+                               ,QtdeAdt
+                               ,QtdeChd
+                               ,ValorDiaria
+                               ,ValorAdicional
+                               ,DataCriacao)
+                         VALUES
+                               (@Id
+                               ,@Descricao
+                               ,@QtdeAdt
+                               ,@QtdeChd
+                               ,@ValorDiaria
+                               ,@ValorAdicional
+                               ,@DataCriacao);";
+
+            ExecutarComando(sql, pTipoUh);
+        }
+
+        public List<TipoUh> Read()
+        {
+            var sql = "SELECT * FROM TipoUh";
+
+            var comando = CriarComando(sql);
+
+            return ObterListaTipoUh(comando);
+        }
+
+        public List<TipoUh> Read(FiltroTipoUH pFiltro)
+        {
+            var sql = "SELECT * FROM TipoUh WHERE ";
+
+            var condicoes = new List<string>();
+
+            var comando = CriarComando(sql);
+
+            if (!string.IsNullOrWhiteSpace(pFiltro.Id))
+            {
+                condicoes.Add("Id = @Id");
+                comando.Parameters.AddWithValue("Id", pFiltro.Id);
+            }
+                
+            if (!string.IsNullOrWhiteSpace(pFiltro.Descricao))
+            {
+                condicoes.Add("Descricao LIKE @Descricao");
+                comando.Parameters.AddWithValue("Descricao", pFiltro.Descricao + "%");
+            }
+
+            sql += string.Join(" AND ", condicoes.ToArray());
+
+            comando.CommandText = sql;
+
+            return ObterListaTipoUh(comando);
+        }
+
+        private List<TipoUh> ObterListaTipoUh(SqlCommand pComando)
+        {
+            var listaTiposUh = new List<TipoUh>();
+
+            using (var reader = pComando.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    var tipoUh = new TipoUh();
+
+                    tipoUh.Id = (Guid)reader["Id"];
+                    tipoUh.Descricao = reader["Descricao"].ToString();
+                    tipoUh.QtdeAdt = Convert.ToInt32(reader["QtdeAdt"]);
+                    tipoUh.QtdeChd = Convert.ToInt32(reader["QtdeChd"]);
+                    tipoUh.ValorDiaria = Convert.ToDouble(reader["ValorDiaria"]);
+                    tipoUh.ValorAdicional = Convert.ToDouble(reader["ValorAdicional"]);
+                    tipoUh.DataCriacao = Convert.ToDateTime(reader["DataCriacao"]);
+
+                    if (reader["DataModificacao"] != DBNull.Value)
+                        tipoUh.DataModificacao = Convert.ToDateTime(reader["DataModificacao"]);
+
+                    listaTiposUh.Add(tipoUh);
+                }
+            }
+
+            return listaTiposUh;
+        }
+
+        public void Update(TipoUh pTipoUh)
+        {
+            //update
+
+            //comando sql de update
+            var sql = @"UPDATE TipoUh SET
+                               Descricao = @Descricao
+                               ,QtdeAdt = @QtdeAdt
+                               ,QtdeChd = @QtdeChd
+                               ,ValorDiaria = @ValorDiaria
+                               ,ValorAdicional = @ValorAdicional
+                               ,DataModificacao = @DataModificacao
+                        WHERE 
+                               Id = @Id";
+
+            ExecutarComando(sql, pTipoUh, EnOperacao.Update);
+        }
+
+        public void Delete(Guid pId)
+        {
+            var sql = @"DELETE FROM TipoUh 
+                        WHERE 
+                            Id = @Id";
+
+            ExecutarComando(sql, new TipoUh() { Id = pId }, EnOperacao.Delete);
+        }
+    }
+}
